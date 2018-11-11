82e0d485d058e9fd88985aead9d8f226f6d8735c
Damyan <DamqnS@gmail.com>
2018-11-11 14:57:28 +0200

    logs doc name

--- routes/api/items.js
+++ routes/api/items.js
@@ -83,7 +83,7 @@ router.get("/:id", (req, res) => {
 	Item.findById(id, removedFields)
 		.exec()
 		.then(doc => {
-			console.log("Retrieved document: \n" + doc);
+			console.log("Retrieved document: \n" + doc.name);
 			if (doc) {
 				res.status(200).json(doc);
 			} else {
