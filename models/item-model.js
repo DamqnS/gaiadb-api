627d88384ead615c18f95a80abd3e56bba10fd99
Damyan <DamqnS@gmail.com>
2018-11-12 20:27:56 +0200

    adds timestamps

--- models/item-model.js
+++ models/item-model.js
@@ -2,57 +2,58 @@ const mongoose = require("mongoose");
 
 // Item schema
 
-const itemschema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		unique: true,
-		index: true
-	},
-	class: {
-		type: String,
-		required: true
-	},
-	type: {
-		type: String,
-		required: true
-	},
-	rarity: {
-		type: String,
-		required: true
-	},
-	level: {
-		type: Number,
-		required: true
-	},
-	description: {
-		type: String
-	},
-	skills: {
-		passive: {
+const itemschema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true
+		},
+		class: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		},
+		rarity: {
+			type: String,
+			required: true
+		},
+		level: {
+			type: Number,
+			required: true
+		},
+		description: {
 			type: String
 		},
-		debuff: {
-			type: String
-		},
-		active: {
-			type: String
-		}
-	},
-	stats: [
-		{
-			_name: {
+		skills: {
+			passive: {
 				type: String
 			},
-			_value: {
-				type: Number
+			debuff: {
+				type: String
+			},
+			active: {
+				type: String
 			}
-		}
-	],
-	added: {
-		type: Date,
-		default: Date.now
+		},
+		stats: [
+			{
+				_name: {
+					type: String
+				},
+				_value: {
+					type: Number
+				}
+			}
+		]
+	},
+	{
+		timestamps: true
 	}
-});
+);
 
 module.exports = mongoose.model("Item", itemschema, "items");
